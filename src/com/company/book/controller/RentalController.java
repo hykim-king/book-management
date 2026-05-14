3f35ecbaaa4c0f0949bb5879b4bacfbb21b28422
장소은 <soeunj168@naver.com>
2026-05-14 16:43:21 +0900

    feat: 대여파일등록

---
+++ src/com/company/book/controller/RentalController.java
@@ -0,0 +1,14 @@
+package com.company.book.controller;
+
+
+public class RentalController {
+ 
+
+    public void processRental() {
+        System.out.println("=== 도서 대여 시스템 ===");
+        
+        // InputUtil을 사용하여 사용자로부터 입력받음
+        System.out.print("대여할 도서 번호: ");
+      
+    }
+}
